09eac6f834cf8fff1fd5bf2d85eef5dddf02e3f0
Zahra1809 <zahragil1809@gmail.com>
2022-03-26 21:03:46 +0700

    inisialisasi project

---
+++ Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_2
+{
+    public class Mahasiswa
+    {
+        //properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("\nNama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //mengeset nilai properties objek saya dan kamu
+            saya.Nim = "12345";
+            saya.Nama = "Jono";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "12346";
+            kamu.Nama = "Paijo";
+            kamu.Ipk = 3.70f;
+
+            //memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+
+        }
+    }
+}
